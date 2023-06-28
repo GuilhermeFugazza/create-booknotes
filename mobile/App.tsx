91b2fe5ea05a22162a92c574341bd413464a7800
BeatrizZanelatto <104568663+BeatrizZanelatto@users.noreply.github.com>
2023-06-28 20:00:45 +0000

    Crreate screen CreateFolders

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -14,6 +14,7 @@ import {
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import { ColorPicker } from 'react-native-color-picker'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,6 +29,13 @@ export default function App() {
       <Loading />
     );
   }
+
+  const App = () => (
+    <ColorPicker
+      onColorSelected={color => alert(`Color selected: ${color}`)}
+      style={{ flex: 1 }}
+    />
+  )
 
   return (
     <>
--- mobile/src/screens/CreateFolders.tsx
+++ mobile/src/screens/CreateFolders.tsx
@@ -1,23 +1,53 @@
-import { Alert, ScrollView, Text, View, TouchableOpacity } from "react-native";
-import { BackButton } from "../components/BackButton";
-import { useNavigation } from '@react-navigation/native';
+import { useState } from "react";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
+import { FontAwesome } from '@expo/vector-icons';
+import { Header } from "../components/Header";
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 export function CreateFolders() {
 
     const { navigate } = useNavigation()
 
     return (
-        <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 100 }}
-            >
-                <BackButton />
+        <View className="flex-1 bg-background pt-16 pb-10">
 
-                <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
-                    "Nome do livro"
-                </Text>
-            </ScrollView>
+            <Header />
+            <Text className="w-full font-semibold text-lg text-white mx-1 text-center">
+                Crie uma pasta
+            </Text>
+            <View className='border border-violet-600 mt-1'></View>
+            <View className="flex justify-center items-center px-12 pt-12 pb-10">
+
+                <TextInput
+                    className="w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
+                    placeholder="Nome..."
+                    placeholderTextColor={colors.zinc[400]}
+                />
+
+                <TextInput
+                    className="w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-zinc-800 focus:border-violet-500"
+                    placeholder="Escolha a cor da pasta"
+                    placeholderTextColor={colors.zinc[400]}
+                />
+
+                <TouchableOpacity
+                    className="w-52 h-14 flex-row items-center justify-center bg-green-500 rounded-md mt-16"
+                    activeOpacity={0.7}
+                //onPress={CreateNewBook}
+                >
+                    <Feather
+                        name="check"
+                        size={20}
+                        color={colors.white}
+                    />
+
+                    <Text className="font-semibold text-base text-white ml-2 ">
+                        Confirmar
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
