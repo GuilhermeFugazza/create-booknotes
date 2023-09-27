1a3f794c23703c87e90d8dee30e0ef469ed3f234
Your Name <you@example.com>
2023-09-27 16:50:15 -0300

    testes

--- mobile/src/screens/CreateBooks.tsx
+++ mobile/src/screens/CreateBooks.tsx
@@ -6,83 +6,104 @@ import { FontAwesome } from '@expo/vector-icons';
 import { Header } from "../components/Header";
 import { api } from "../lib/axios";
 
+import { PrismaClient } from '@prisma/client';
 
-export function New() {
-  const [title, setTitle] = useState('');
-  const [autor, setAutor] = useState('');
-  const [editora, setEditora] = useState('');
+const prisma = new PrismaClient();
 
-  async function CreateNewBook() {
+export function BookButton() {
+  const [bookDetails, setBookDetails] = useState({
+    name: '',
+    title: '',
+    pages: '',
+  });
+
+  const handleBookDetailsChange = (fieldName: string, value: string) => {
+    setBookDetails({
+      ...bookDetails,
+      [fieldName]: value,
+    });
+  };
+
+  const handleCreateBook = async () => {
     try {
-      await api.post('/book', { title, editora, autor })
-
-      setTitle('');
-      setEditora('');
-      setAutor('');
-
-      Alert.alert('Novo livro adicionado', 'Livro adicionado com sucesso!');
+      await prisma.books.create({
+        data: {
+          title: bookDetails.title,
+          author: bookDetails.name,
+          pcompany: '', // Preencha conforme necessário
+          favorite: false, // Preencha conforme necessário
+        },
+      });
+      // Limpe os campos do formulário após a criação do livro
+      setBookDetails({
+        name: '',
+        title: '',
+        pages: '',
+      });
+      console.log('Livro cadastrado com sucesso!');
     } catch (error) {
-      console.log(error)
-      Alert.alert('Ops', 'Não foi possível adicionar o livro')
+      console.error('Erro ao cadastrar livro:', error);
     }
-  }
+  };
 
-  return (
-    <View className="flex-1 bg-background pt-16 pb-10">
-      
-        <Header />
-        <Text className="w-full font-semibold text-lg text-white mx-1 text-center">
-          Adicione um livro
-        </Text>
-        <View className='border border-violet-600 mt-1'></View>
-        <View className="flex justify-center items-center px-12 pt-12 pb-10">
-
-          <TextInput
-            className="w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
-            placeholder="Título..."
-            placeholderTextColor={colors.zinc[400]}
-          />
-
-          <TextInput
-            className=" w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
-            placeholder="Nome..."
-            placeholderTextColor={colors.zinc[400]}
-          />
-
-          <TextInput
-            className="w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
-            placeholder="Numero de páginas..."
-            placeholderTextColor={colors.zinc[400]}
-          />
-
-          <TouchableOpacity
-            className="w-28 h-40 items-center justify-center bg-zinc-900 rounded-lg m-6 border-2 border-zinc-800"
-            activeOpacity={0.7}
-            //onPress={() => navigate('photo')}
-          >
-
-            <Text className="font-semibold text-base text-zinc-400 m-1 text-center pb-3">
-              Adicione uma capa para o livro
-            </Text>
-            <FontAwesome name="camera" size={24} color={colors.violet[500]} />
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            className="w-52 h-14 flex-row items-center justify-center bg-green-500 rounded-md mt-16"
-            activeOpacity={0.7}
-            onPress={CreateNewBook}
-          >
-            <Feather
-              name="check"
-              size={20}
-              color={colors.white}
+    return (
+      <View className="flex-1 bg-background pt-16 pb-10">
+        
+          <Header />
+          <Text className="w-full font-semibold text-lg text-white mx-1 text-center">
+            Adicione um livro
+          </Text>
+          <View className='border border-violet-600 mt-1'></View>
+          <View className="flex justify-center items-center px-12 pt-12 pb-10">
+  
+            <TextInput
+              className="w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
+              placeholder="Título..."
+              placeholderTextColor={colors.zinc[400]}
             />
-
-            <Text className="font-semibold text-base text-white ml-2 ">
-              Confirmar
-            </Text>
-          </TouchableOpacity>
-        </View>
-    </View>
-  )
+  
+            <TextInput
+              className=" w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
+              placeholder="Nome..."
+              placeholderTextColor={colors.zinc[400]}
+            />
+  
+            <TextInput
+              className="w-full h-12 pl-4 rounded-lg mt-6 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-violet-500"
+              placeholder="Numero de páginas..."
+              placeholderTextColor={colors.zinc[400]}
+            />
+  
+            <TouchableOpacity
+              className="w-28 h-40 items-center justify-center bg-zinc-900 rounded-lg m-6 border-2 border-zinc-800"
+              activeOpacity={0.7}
+              //onPress={() => navigate('photo')}
+            >
+  
+              <Text className="font-semibold text-base text-zinc-400 m-1 text-center pb-3">
+                Adicione uma capa para o livro
+              </Text>
+              <FontAwesome name="camera" size={24} color={colors.violet[500]} />
+            </TouchableOpacity>
+  
+            <TouchableOpacity
+              className="w-52 h-14 flex-row items-center justify-center bg-green-500 rounded-md mt-16"
+              activeOpacity={0.7}
+              onPress={handleCreateBook}
+            >
+              <Feather
+                name="check"
+                size={20}
+                color={colors.white}
+              />
+  
+              <Text className="font-semibold text-base text-white ml-2 ">
+                Confirmar
+              </Text>
+            </TouchableOpacity>
+          </View>
+      </View>
+    )
 }
+
+  
